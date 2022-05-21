29e237897e5b25cf69f333c58710c7e09c88c2e0
ericfg19 <eric_fg@live.com.ar>
2022-05-21 09:36:08 -0300

    add exercises

---
+++ hackerrank_exercises/BirthdayCake.java
@@ -0,0 +1,68 @@
+package Exercícios;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        
+        int maxCandlesHeight = Integer.MIN_VALUE;
+        int maxCandlesCount = 0;
+    
+        for(int i=0; i < candles.size(); candles++ ) {
+            if(candles.get(i) == maxCandlesHeight){
+                maxCandlesCount++;
+            }
+            if(candles.get(i) > maxCandlesHeight){
+                maxCandlesHeight = candles.get(i);
+                maxCandlesCount = 1;
+            }
+            
+            return maxCandlesCount;
+            
+        }
+    }
+    
+    
+}
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int maxcandlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ hackerrank_exercises/MinMaxSum.java
@@ -0,0 +1,42 @@
+package Exercícios;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+        Collections.sort(arr);//1 2 3 4 5
+        long sum2 = arr.stream().skip(1).mapToLong(Long::valueOf).sum();
+        long sum1 = arr.stream().limit(arr.size()-1).mapToLong(Long::valueOf).sum();
+        System.out.println(sum1+" "+sum2);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
+
